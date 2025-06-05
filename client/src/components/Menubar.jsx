12f009155d81f4c1442626529fecc34cc787e315
dea-2382 <demi22ise@cmrit.ac.in>
2025-06-05 12:01:03 +0530

    debuggin bad response

--- client/src/components/Menubar.jsx
+++ client/src/components/Menubar.jsx
@@ -52,7 +52,7 @@ const Menubar = () => {
           <div className="bg-dark text-white rounded-circle d-flex justify-content-center align-items-center"
                style={{ width: 40, height: 40, cursor: "pointer" }}
                onClick={() => setOpen(prev => !prev)}>
-            {userData?.name?.charAt(0)?.toUpperCase() || "U"}
+            {userData?.name?.charAt(0)?.toUpperCase()}
 
           </div>
 
